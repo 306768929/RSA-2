9f15363b8fb64377ec7697138e09536af4581178
Emre Macit <emre@ideasoft.com.tr>
2013-05-27 17:00:20 +0300

    missed example completed :)

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 include('RSA.php');
 //or use with namespace + autoload .
 $RSA = new RSA();
-
+$keys = $RSA->generateKeys ('9990454949', '9990450271');
 $message="your test message goes here";
 $encoded = $RSA->encrypt ($message, 5);
 $decoded = $RSA->decrypt ($encoded);
